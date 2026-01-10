efe1c48f30011de2df1f878dd604f383996ef5ec
khanh <doquangkhanh2@gmail.com>
2026-01-10 09:43:17 +0700

    refactor prj.conf

--- peripheral_uart/build/peripheral_uart/zephyr/include/generated/app_commit.h
+++ peripheral_uart/build/peripheral_uart/zephyr/include/generated/app_commit.h
@@ -6,7 +6,7 @@
  * alternatively user defined BUILD_VERSION.
  */
 
-#define APP_COMMIT                   e2a97fe2578a
-#define APP_COMMIT_STRING            "e2a97fe2578a"
+#define APP_COMMIT                   f3dbb4a1d453
+#define APP_COMMIT_STRING            "f3dbb4a1d453"
 
 #endif /* _APP_COMMIT_H_ */
